06a5bee6d62fff960a2f1c022888a30c347ca9ca
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2024-03-19 18:57:18 +0200

    make build

---
+++ index.e3add687.js
@@ -0,0 +1,2 @@
+function e(){document.querySelector(".page__body").classList.remove("disable-scroll")}document.querySelector("#prevent").addEventListener("click",function(){document.querySelector(".page__body").classList.add("disable-scroll")}),document.querySelectorAll(".allow").forEach(function(c){c.addEventListener("click",e)});
+//# sourceMappingURL=index.e3add687.js.map
